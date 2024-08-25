6666df327a413056632b5eef1363653759abc7c8
Sneha kumari <snehasharma7620@gmail.com>
2024-08-26 06:06:02 +0800

    Initialized inputs for the calculator program

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -3,4 +3,23 @@ use std::io;
 
 fn main() {
     println!("Start of the calculator program");
+
+    println!("Let's implement the basic functions addition, subtraction, division and multiplication first with two inputs")
+
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    let mut oper = String::new();
+
+    println!("Please input the first number")
+    io::stdin().read_line(&mut input1).expect("Invalid input");
+
+    println!("Let's input the second number");
+    io::stdin().read_line(&mut input2).expect("Invalid input");
+
+
+
+    fn addition(input1 | input2){
+        
+    }
+    
 }

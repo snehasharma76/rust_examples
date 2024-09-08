42d14f624bc1bf5b8d70d2ad643b332a7f933b62
Sneha kumari <snehasharma7620@gmail.com>
2024-09-08 15:45:52 +0800

    Calculator done

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -6,39 +6,60 @@ fn main() {
 
     println!("Let's implement the basic functions addition, subtraction, division and multiplication first with two inputs");
 
-    let mut input1 = String::new();
-    let mut input2 = String::new();
-    let mut oper = String::new();
-    let result = i32;
+    let mut input1:String = String::new();
+    let mut input2: String = String::new();
+    let mut oper: String = String::new();
+    let result: i32;
 
     println!("Please input the first number");
     io::stdin().read_line(&mut input1).expect("Invalid input");
+    let input1: i32 = match input1.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input!");
+            return;
+        }
+    };
+
 
     println!("Let's input the second number");
     io::stdin().read_line(&mut input2).expect("Invalid input");
+    let input2: i32 = match input2.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input!");
+            return;
+        }
+    };
 
-    println!("List of operations to be performed");
-    println!("1. Addition");
-    println!("(2) Subtraction");
-    println!("(3) Multplication");
-    println!("(4) Division");
-    println!("Select the oepration to be performed!");
-    io::stdin().read_line(&mut oper).expect("Invalid input");
+    println!("List of operators:");
+    println!("(1) Add");
+    println!("(2) Subtract");
+    println!("(3) Multiply");
+    println!("(4) Divide");
+    println!("Select the number associated with the desired operation: ");
 
-    //need to figure out why is it not working as string here 
+    io::stdin().read_line(&mut oper).expect("Invalid Input");
+    let oper: i32 = match oper.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input!");
+            return;
+        }
+    };
+
     match oper {
-        1 => result = input1 + &input2,
-        2 => result = input1 - &input2,
-        3 => result = input1 * &input2,
-        4 => result = input1 / &input2,
-        - => {
-            println!( "Invalid Selection");
+        1 => result = input1 + input2,
+        2 => result = input1 - input2,
+        3 => result = input1 * input2,
+        4 => result = input1 / input2,
+        _ => {
+            println!("Invalid selection");
             return;
         }
     }
-    println!("The result is: {}", result);
-    //fn addition(input1 | input2){
-        
-    }
-    
 
+    println!("The result is: {}", result);
+}
+
+   
---
+++ calculator/target/debug/deps/calculator-60570a499b02abb2.d
@@ -0,0 +1,5 @@
+/Users/sneha/rust_examples/calculator/target/debug/deps/calculator-60570a499b02abb2: src/main.rs
+
+/Users/sneha/rust_examples/calculator/target/debug/deps/calculator-60570a499b02abb2.d: src/main.rs
+
+src/main.rs:
